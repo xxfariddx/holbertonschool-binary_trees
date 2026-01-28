a11953c1cda48ee8e4487b89461039f97146660f
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-28 09:41:31 +0000

    task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    if (node == NULL)
+        return (0);
+
+    if (node->parent == NULL && (node->left != NULL | node->right != NULL))
+        return (1);
+
+    return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -29,5 +29,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 /* checks if the node is leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
+/* checks if the node is root */
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif
