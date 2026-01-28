810a4fdd89e6f639ef756e34f97aa007a3c76092
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-28 10:28:52 +0000

    imroved version of logic and betty style

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,20 +1,17 @@
 #include "binary_trees.h"
 #include <stdio.h>
 
+/**
+ * binary_tree_inorder - Goes through a binary tree using inorder traversal
+ * @tree: Pointer to the root node of the tree
+ * @func: Pointer to a function to call for each node
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+	if (tree == NULL || func == NULL)
+		return;
 
-    binary_tree_inorder(tree->left, func);
-
-    if (tree->right != NULL)
-        func(tree->n);
-
-    binary_tree_inorder(tree->right, func);
-    
-    if (tree->right != NULL)
-        return;
-    
-    func(tree->n);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
