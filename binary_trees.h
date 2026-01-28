ca853543e34029e1cc02bf8026c549c172661c82
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-28 11:00:06 +0000

    task 9

--- binary_trees.h
+++ binary_trees.h
@@ -37,5 +37,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 /* prints post-order traversal */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+/* returns the height of binary tree */
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif
