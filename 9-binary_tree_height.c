e5ba8f79a2623c0330dd996c04b95738f4e56ac2
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-01-28 11:08:28 +0000

    right logic

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,6 +17,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
+    if (tree->parent == NULL){
+        if (left_height > right_height)
+		    return (left_height);
+	    return (right_height);
+    }
+
 	if (left_height > right_height)
 		return (left_height + 1);
 	return (right_height + 1);
